e259fbd96d06b6ad1df76cc33bb551a7f4c58b96
Antonio <antoniowellington542@gmail.com>
2025-03-16 11:50:56 -0300

    lib ui example

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    outDir: "dist",
+    format: ["cjs", "esm"],
+    dts: true,
+    minify: true,
+    sourcemap: true,
+    clean: true,
+    tsconfig: "tsconfig.build.json"
+})
